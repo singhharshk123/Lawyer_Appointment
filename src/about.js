e4630f9576dfbf21cf74eaef3333e274ba917538
Harsh Singh <singhharshk123@gmail.com>
2022-03-23 22:53:48 +0530

    navbar completed

---
+++ src/about.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function About () {
+  return (
+    <div className="App">
+      <Navbar />
+      djkfbdhfvbdscnbdf,mbvcfkldjncb,djfvfvfvfv
+
+      dfnfvhbfkjvfkjvjf
+
+      <h1>ebjdfgfkgfkjfkjbkjffkurgfi</h1>
+      
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import { faBars, faTimeline } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+// import Button from "./Button";
+import menuItems from "./MenuItems";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">
+        React <i className="fab fa-react"></i>
+      </h1>
+      <div className="menu-icon" onClick={handleClick}>
+        {/* <FontAwesomeIcon className={ active ? "fas fa-times" : "fas fa-bars"} icon={ active ? faTimeline : faBars} /> */}
+
+        {
+            active ? <FontAwesomeIcon className="fas fa-times" icon={faTimeline} />
+            : <FontAwesomeIcon className="fas fa-bars" icon={faBars} />
+        }
+
+      </div>
+      <ul className={active ? "nav-menu active" : "nav-menu"}>
+        {menuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={item.url} className={item.cName}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <button>SIGN UP</button>
+    </nav>
+  );
+};
+
+export default Navbar;
